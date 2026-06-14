8666b6a904ed192b593484047c1963e6c4a01991
Ferez <minh9a4@gmail.com>
2026-06-14 14:28:03 +0700

    feat: add solution for LC 859 - Buddy Strings
    - Implemented the algorithm to determine if two strings can be made equal by swapping two characters.
    - Included helper functions to find differing indexes and check for repeated characters.
    - Added a main function with test cases for validation.

---
+++ OneEasyProblemEveryDay/2026-06-14/LC-859-buddy-strings/main.go
@@ -0,0 +1,68 @@
+package main
+
+// LC 859. Buddy Strings
+// Link: https://leetcode.com/problems/buddy-strings/
+//
+// Approach:
+//   [write your thinking here before coding]
+//
+// Time:  O(?)
+// Space: O(?)
+
+import "fmt"
+
+func buddyStrings(s string, goal string) bool {
+	if len(s) != len(goal) {
+		return false
+	}
+	diffIndexes := getDiffIndexes(s, goal)
+	switch len(diffIndexes) {
+	case 2:
+		i, j := diffIndexes[0], diffIndexes[1]
+		if s[i] == goal[j] && s[j] == goal[i] {
+			// swap i <=> j is correct
+			return true
+		} else {
+			return false
+		}
+	case 0:
+		// two string is identical, check any
+		if hasRepeatedCharacter(s) {
+			// swap keep the string remains the same
+			return true
+		} else {
+			return false
+		}
+	default:
+		return false
+	}
+}
+
+// precondidtion: len(s1) == len(s2)
+func getDiffIndexes(s1, s2 string) []int {
+	indexes := make([]int, 0)
+	for i := range len(s1) {
+		if s1[i] != s2[i] {
+			indexes = append(indexes, i)
+		}
+	}
+	return indexes
+}
+
+func hasRepeatedCharacter(s string) bool {
+	dict := make(map[rune]bool)
+	for _, ch := range s {
+		if dict[ch] {
+			return true
+		} else {
+			dict[ch] = true
+		}
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(buddyStrings("ab", "ba")) // expected: true
+	fmt.Println(buddyStrings("ab", "ab")) // expected: false
+	fmt.Println(buddyStrings("aa", "aa")) // expected: true
+}
